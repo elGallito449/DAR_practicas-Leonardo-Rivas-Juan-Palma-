636ac363129e8260b5b822cd2745b41ad4ae746f
elGallito449 <juanpp010605@gmail.com>
2026-04-06 19:31:55 +0200

    Update Tarea.java

--- src/servidortareaspetd/Tarea.java
+++ src/servidortareaspetd/Tarea.java
@@ -2,19 +2,6 @@ package servidorpetd;
 
 // objeto que guarda info y viaja por la cola
 
-/* PQ string??
---> Para el id ("0001"): Si usáramos un int, el número 0001 se
-guardaría simplemente como 1, perdiendo los ceros a la izquierda.
-Como el ABNF nos obliga a que sean 4 dígitos exactos, guardarlo 
-como texto (String) es lo más seguro
-
---> Para los parametros ("10"): Cuando leemos datos de un enchufe
-de red (socket), lo que nos llega es texto puro. Es mucho más 
-seguro guardarlo en la tarea tal cual llega como texto (String). 
-Más adelante, justo en el momento de calcular el factorial, ya 
-lo transformaremos a número entero.
-
-*/
 
 public class Tarea {
  
