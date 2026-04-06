2a317fe8e45824c60a6c3b1ca177644d52db19d7
elGallito449 <juanpp010605@gmail.com>
2026-04-06 19:31:14 +0200

    Update GestorCola.java

--- src/servidortareaspetd/GestorCola.java
+++ src/servidortareaspetd/GestorCola.java
@@ -3,27 +3,10 @@ package servidorpetd;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.LinkedBlockingQueue;
 
-/*
- * CONCEPTO: Thread (Hilo)
- * Al hacer que nuestra clase "extends Thread", la convertimos en un mini-programa 
- * que se ejecuta en paralelo al resto del servidor. 
- * ¿Por qué es vital aquí? Porque mientras este hilo está "atascado" calculando 
- * un factorial muy largo, el hilo principal del servidor sigue libre para 
- * escuchar a nuevos clientes o responder a un comando POLL.
- */
+
 public class GestorCola extends Thread {
 
     private LinkedBlockingQueue<Tarea> cola;
-    
-    /*
-     * CONCEPTO: ConcurrentHashMap
-     * Un HashMap normal es como un diccionario (guardas una Tarea asociada a su ID).
-     * El problema es que si el hilo del cliente intenta meter una tarea nueva 
-     * justo en el mismo milisegundo en el que este hilo Worker intenta leer otra, 
-     * el programa puede crashear (lo que en teoría llamamos "condición de carrera").
-     * La versión "Concurrent" gestiona los semáforos internamente de forma automática 
-     * para que sea 100% seguro leer y escribir desde varios hilos a la vez.
-     */
     private ConcurrentHashMap<String, Tarea> registro;
 
     public GestorCola() {
@@ -43,14 +26,6 @@ public class GestorCola extends Thread {
     }
 
     // --- HILO PLANIFICADOR (WORKER) ---
-
-    /*
-     * CONCEPTO: @Override (Sobrescribir)
-     * La clase padre "Thread" ya tiene un método run() vacío por defecto. 
-     * Con @Override le decimos a Java: "Ignora el método original, quiero que 
-     * uses mi propia versión". Todo el código que metamos dentro de este run() 
-     * es lo que se ejecutará en paralelo cuando arranquemos el hilo.
-     */
     @Override
     public void run() {
         while (true) {
@@ -69,7 +44,6 @@ public class GestorCola extends Thread {
         }
     }
 
-    // --- LÓGICA DE LAS TAREAS ---
 
 // --- LÓGICA DE LAS TAREAS ---
 
@@ -92,7 +66,7 @@ public class GestorCola extends Thread {
                 return; // Salimos antes
             }
             
-            t.setEstadoActual(Tarea.STATE_FINISHED); // Si todo fue bien
+            t.setEstadoActual(Tarea.STATE_FINISHED); // Si todo ha ido bien
 
         } catch (NumberFormatException e) {
             // Integer.parseInt() fallará si nos mandan "PUSH FACTORIAL letras"
